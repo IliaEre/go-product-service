961abfd64ce3f8612179d57a66b80dd3ccf3dc58
Ilia Ivankin <ilia_ivankin@epam.com>
2021-12-01 00:27:34 +0300

    adde swagger

--- go.mod
+++ go.mod
@@ -5,11 +5,31 @@ go 1.17
 require github.com/aws/aws-xray-sdk-go v1.6.0
 
 require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/felixge/httpsnoop v1.0.2 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.6 // indirect
+	github.com/go-openapi/spec v0.20.4 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/swaggo/files v0.0.0-20210815190702-a29dd2bc99b2 // indirect
+	github.com/swaggo/http-swagger v1.1.2 // indirect
+	github.com/swaggo/swag v1.7.6 // indirect
+	github.com/urfave/cli/v2 v2.3.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.27.0 // indirect
 	go.opentelemetry.io/otel/internal/metric v0.25.0 // indirect
 	go.opentelemetry.io/otel/metric v0.25.0 // indirect
+	golang.org/x/tools v0.1.7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
 
 require (
@@ -32,9 +52,9 @@ require (
 	go.opentelemetry.io/otel/sdk v1.2.0 // indirect
 	go.opentelemetry.io/otel/trace v1.2.0 // indirect
 	go.opentelemetry.io/proto/otlp v0.10.0 // indirect
-	golang.org/x/net v0.0.0-20210226101413-39120d07d75e // indirect
-	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
-	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/net v0.0.0-20211123203042-d83791d6bcd9 // indirect
+	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/genproto v0.0.0-20210114201628-6edceaf6022f // indirect
 	google.golang.org/grpc v1.42.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
--- main.go
+++ main.go
@@ -13,13 +13,31 @@ import (
 	"github.com/aws/aws-xray-sdk-go/xray"
 	"github.com/gorilla/mux"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/gorilla/mux/otelmux"
+
+	httpSwagger "github.com/swaggo/http-swagger"
 )
 
+// GetProducts godoc
+// @Summary Get details of products
+// @Description Get details of products
+// @Tags products
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} Order
+// @Router /product/all [get]
 func findAll(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(Products)
 }
 
+// GetProducts godoc
+// @Summary Get details of product
+// @Description Get details of product
+// @Tags products
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} Order
+// @Router /product/{id} [get]
 func findOne(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	vars := mux.Vars(r)
@@ -35,6 +53,15 @@ func findOne(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// CreateOrder godoc
+// @Summary Create a new product
+// @Description Create a new product with the input paylod
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Param order body Order true "Create product"
+// @Success 200 {object} Order
+// @Router /product [post]
 func create(w http.ResponseWriter, r *http.Request) {
 	requestBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -48,19 +75,64 @@ func create(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(product)
 }
 
+// CreateOrder godoc
+// @Summary Update a new product
+// @Description Update a selected product with the input paylod
+// @Tags orders
+// @Accept  json
+// @Produce  json
+// @Param order body Order true "Create order"
+// @Success 200 {object} Order
+// @Router /product/{id}/add [put]
+func update(w http.ResponseWriter, r *http.Request) {
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println(requestBody, "Problem with body!")
+	}
+
+	vars := mux.Vars(r)
+	key, err := strconv.ParseInt(vars["id"], 10, 32)
+	if err != nil {
+		log.Println("problem with id...", err)
+	}
+
+	w.Header().Add("Content-Type", "application/json")
+	var product Product
+	json.Unmarshal(requestBody, &product)
+
+	for index, doc := range Products {
+		if int64(doc.Id) == key {
+			Products[index] = product
+			json.NewEncoder(w).Encode(product)
+		}
+	}
+}
+
+// @title Orders API
+// @version 1.0
+// @description This is a sample serice for managing orders
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8082
+// @BasePath /
 func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.Use(otelmux.Middleware("my-api"))
 	xraySegment := xray.NewFixedSegmentNamer("aws-go-service")
+
+	myRouter.PathPrefix("/swagger").Handler(httpSwagger.WrapHandler)
 
 	myRouter.Handle("/api/health", xray.Handler(xraySegment,
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			json.NewEncoder(w).Encode(map[string]interface{}{"ok": true})
 		})))
 
+	myRouter.Handle("/product", xray.Handler(xraySegment, http.HandlerFunc(create)))
 	myRouter.Handle("/product/all", xray.Handler(xraySegment, http.HandlerFunc(findAll)))
 	myRouter.Handle("/product/{id}", xray.Handler(xraySegment, http.HandlerFunc(findOne)))
-	myRouter.Handle("/product/{id}/add", xray.Handler(xraySegment, http.HandlerFunc(create)))
+	myRouter.Handle("/product/{id}/add", xray.Handler(xraySegment, http.HandlerFunc(update)))
 	http.ListenAndServe(":8082", myRouter)
 }
 
@@ -71,7 +143,7 @@ func init() {
 		{Id: 2, Name: "Useful laptop", Description: "khm..."},
 	}
 
-	log.Println("init x-ray configudaration")
+	log.Println("init x-ray configuration")
 	s, _ := sampling.NewCentralizedStrategyWithFilePath("rules.json")
 	xray.Configure(xray.Config{SamplingStrategy: s})
 	log.Println("end of init")
