0db7bfd826c70d25c1716f475cd2f9979a35c3a6
ilya <ilya.selfrealization@gmail.com>
2021-12-06 09:14:56 +0300

    added lambda

---
+++ order-lambda/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sns"
+
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+var topic = "orderTopic"
+var svc *sns.SNS
+
+type OrderEvent struct {
+	Name      string `json:"name"`
+	ProductId string `json:"productId"`
+}
+
+func HandleRequest(ctx context.Context, order OrderEvent) (string, error) {
+	json, err := json.Marshal(order)
+	if err != nil {
+		log.Printf("Error marshalling order")
+		return "", err
+	}
+
+	message := string(json)
+
+	result, err := svc.Publish(&sns.PublishInput{
+		Message:  &message,
+		TopicArn: &topic,
+	})
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println(*result.MessageId)
+	return fmt.Sprintf("Order was created with name: %s!", order.Name), nil
+}
+
+func init() {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	svc = sns.New(sess)
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
